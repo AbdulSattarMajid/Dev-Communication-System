08d9775abfebf4aec3f0a8f55663e8c65a6f998d
AbdulSattarMajid <malikabdulsattarmajid@gmail.com>
2025-07-15 09:22:40 +0500

    starting

--- Frontend/project-management/src/App.jsx
+++ Frontend/project-management/src/App.jsx
@@ -2,10 +2,14 @@ import React from "react";
 // import Sidebar from "./Sidebar";
 import Login from "./Authentication/Signin";
 import Sidebar from "./Authentication/Sidebar";
+import TopNavbar from "./Authentication/TopNavbar";
 export default function App() {
   return (
-    <div className="">
+    <div className="flex flex-col">
+      <TopNavbar/>
+
       <Sidebar/>
+
     </div>
   );
 }
---
+++ Frontend/project-management/src/Authentication/TopNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TopNavbar() {
+  return (
+    <div className="bg-blue-400 h-[4rem]">
+
+    </div>
+  )
+}
+
+export default TopNavbar
